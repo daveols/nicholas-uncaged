f6907fbaefb680129f5077ec56e96248017f3869
Dave Olsen <daivols@gmail.com>
2018-05-04 17:00:14 +1000

    NOT THE BEES... ANYTHING BUT THE BEES

--- App.js
+++ App.js
@@ -20,12 +20,32 @@ const Cages = [
   {
     image: 'https://i.pinimg.com/originals/af/a9/3e/afa93eed58d0e4d014b9e9ef5a11a0fe.png',
     quote: 'I love pressure. I eat it for breakfast.'
+  },
+  {
+    image: 'https://cdn.flickeringmyth.com/wp-content/uploads/2018/01/nicolas-cage-ghost-rider-transformation-208622-600x300.jpg',
+    quote: 'Shoot him again... His soul\'s still dancing.'
+  },
+  {
+    image: 'http://1.bp.blogspot.com/-4x8LvBUopUg/UP_3v-hRgcI/AAAAAAAAC90/rerm6FhEJ4I/s1600/Anthony+Lamb+-+Nick+Cage+as+Salvador+Dali.jpg',
+    quote: 'What are these fuckin\' iguanas doing on my coffee table?'
+  },
+  {
+    image: 'http://flowjournal.org/wp-content/uploads/2011/05/Nic-Cage-Bad-Lieutenant.png',
+    quote: 'Everything I take is prescription - except for the heroin.'
+  },
+  {
+    image: 'https://www.thefamouspeople.com/profiles/images/nicolas-cage-2.jpg',
+    quote: 'I love pressure. I eat it for breakfast.'
+  },
+  {
+    image: 'https://www.space.ca/wp-content/uploads/2015/12/Not-the-Bees.jpg',
+    quote: 'OH, NO! NOT THE BEES! NOT THE BEES! AAAAAHHHHH! OH, THEY\'RE IN MY EYES! MY EYES! AAAAHHHHH! AAAAAGGHHH!'
   }
 ]
 
 export default class App extends Component {
   state = {
-    index: 0
+    index: 6
   }
 
   onNext = () => {
@@ -47,9 +67,11 @@ export default class App extends Component {
 
     return (
       <View style={styles.container} accessibilityLabel="testview">
-        <Image style={{ height: 400, width: 400 }} source={{ uri: image }} />
+        <Image style={styles.image} resizeMode="contain" source={{ uri: image }} />
         <Text style={styles.quote} accessibilityLabel="quote">{formattedQuote}</Text>
-        <Button onPress={this.onNext} title="More wisdom" accessibilityLabel="button" />
+        <View style={styles.button}>
+          <Button onPress={this.onNext} title="More wisdom" accessibilityLabel="button" />
+        </View>
       </View>
     );
   }
@@ -60,11 +82,21 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'black',
+  },
+  image: {
+    flex: 1,
+    height: 400,
+    width: 400
   },
   quote: {
-    padding: 20,
+    padding: 10,
+    paddingHorizontal: 20,
     fontSize: 16,
-    textAlign: 'center'
+    textAlign: 'center',
+    color: 'white'
+  },
+  button: {
+    margin: 20
   }
 });
